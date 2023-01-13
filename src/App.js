a92f62cc2aa59f256336d3a6302fb2196af36118
sariamancharla <93614345+sariamancharla@users.noreply.github.com>
2023-01-13 21:48:13 +0100

    Flip Cards. Track the Turns.Track Matched status.
    Flip Cards. Track the Turns.Track Matched status.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { useEffect, useState } from 'react'
 import  SingleCard from './components/SingleCard'
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { "src": "/img/helmet-1.png",matched:false },
+  { "src": "/img/potion-1.png",matched:false },
+  { "src": "/img/ring-1.png",matched:false },
+  { "src": "/img/scroll-1.png" ,matched:false},
+  { "src": "/img/shield-1.png",matched:false },
+  { "src": "/img/sword-1.png" ,matched:false},
 ]
 
 function App() {
@@ -17,6 +17,7 @@ function App() {
   const [turns, setTurns]=useState(0)
   const [choiceOne, setChoiceOne]=useState(null)
   const [choiceTwo, setChoiceTwo]=useState(null)
+  const [disabled, setdisabled]=useState(false)
   //Shuffle Cards
 
   const shuffleCards=()=>{
@@ -25,6 +26,9 @@ function App() {
         .sort(()=>Math.random()-0.5)//sometimes negative sometimes positive
         .map((card)=>({...card,id:Math.random()}))
 
+        setChoiceOne(null)
+        setChoiceTwo(null)
+        
         setCards(shuffledCards)
         setTurns(0)
   }
@@ -32,19 +36,27 @@ function App() {
     //handle a choice
     const handleChoice=(card)=>{
      choiceOne? setChoiceTwo(card): setChoiceOne(card)
-     console.log(card)
           
     }
 
-    useEffect(()=>{
+    useEffect(()=>{      
       if (choiceOne && choiceTwo) {       
+        setdisabled(true)
         if (choiceOne.src===choiceTwo.src){
-          console.log("Cards Match")
+          setCards(prevCards=>{
+            return prevCards.map(card=>{
+              if (card.src===choiceOne.src){
+                return {...card,matched:true}
+              }else{
+                return card
+              }
+            })
+          })
+          resetTurn()
         }
-        else{
-          console.log("Cards No Match")
-        }
-        resetTurn()
+      else{
+        setTimeout(()=>resetTurn(),1000)//wait a second and fire this function
+      }        
     } 
     },[choiceOne,choiceTwo])
 
@@ -52,7 +64,14 @@ function App() {
       setChoiceOne(null)
       setChoiceTwo(null)
       setTurns(prevTurns=>prevTurns+1)
+      setdisabled(false)
     }
+
+  //To start game automatically
+  useEffect(()=>{
+    shuffleCards()
+  },[])
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -62,10 +81,13 @@ function App() {
             <SingleCard
               key={card.id} 
               card={card}
-              handleChoice={handleChoice}            
+              handleChoice={handleChoice} 
+              flipped={card===choiceOne || card===choiceTwo || card.matched}  
+              disabled={disabled}         
             />
         ))}
       </div>
+      <p>Turns:{turns}</p>
     </div>    
   );
 }
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import "./SingleCard.css"
 
-export default function SingleCard({card,handleChoice}) {
+export default function SingleCard({card,handleChoice,flipped,disabled}) {
         
 const handleClick=()=>{
-    handleChoice(card)
+    if(!disabled){
+        handleChoice(card)
     }
+}
 
   return (
     <div className="card" key={card.id}>
-        <div>
+        <div className={flipped?"flipped":""}>
             <img className="front"  src={card.src} alt="card-front"/>
             <img className="back" onClick={handleClick} src="/img/cover.png" alt="card back" />
         </div>
